92c47f8ab47ce41c2defae6ac992adf84418ee04
Hans-Christoph Steiner <hans@eds.org>
2011-06-29 15:44:56 -0400

    ported to a Vendapin class

--- vendapin.py
+++ vendapin.py
@@ -16,229 +16,241 @@ class NakException(Exception):
         Exception.__init__(self, message)
         self.code = code
 
+class Vendapin():
+    '''control a Vendapin card dispenser via USB-serial'''
 
-# packet breakdown:
-# <STX><ADD><CMD><LEN><DTA><ETX><CHK>
-#
-STX = 0x02  # Start of Text Data
-# CMD - Command Code Byte (0x80-0xFF)
-# LEN - Length of Data - Byte Size (0x00 or higher)
-# DTA Packet Data (Optional, leave empty if not used)
-ADD = 0x01  # Device Address (Set to 0x01 if not used)
-ETX = 0x03  # End of Text Data
-# CHK - XOR Checksum of data packet
+    # packet breakdown:
+    # <STX><ADD><CMD><LEN><DTA><ETX><CHK>
+    #
+    STX = 0x02  # Start of Text Data
+    # CMD - Command Code Byte (0x80-0xFF)
+    # LEN - Length of Data - Byte Size (0x00 or higher)
+    # DTA Packet Data (Optional, leave empty if not used)
+    ADD = 0x01  # Device Address (Set to 0x01 if not used)
+    ETX = 0x03  # End of Text Data
+    # CHK - XOR Checksum of data packet
 
-PACKET_MIN = 5 # the maximum packet length
-PACKET_MAX = 128 # the maximum packet length
+    PACKET_MIN = 5 # the maximum packet length
+    PACKET_MAX = 128 # the maximum packet length
 
-# commands
-ENABLE = 0xFE   # Enable VCB-2 Motor Control (Default at start-up)
-DISABLE = 0xFF  # Disable VCB-2 Motor Control
-RESERVED = 0xF0 # Reserved - for Factory use only
+    # commands
+    ENABLE = 0xFE   # Enable VCB-2 Motor Control (Default at start-up)
+    DISABLE = 0xFF  # Disable VCB-2 Motor Control
+    RESERVED = 0xF0 # Reserved - for Factory use only
 
-DISPENSE = 0x80            # Dispense Card - no data
-REQUEST_STATUS = 0x81      # Request Status - no data
-READ_TOTAL_COUNT = 0x82    # Read Total Dispense Count Meter (count after API command is sent)
-READ_TOTAL_BUTTON_COUNT = 0x83  # Read Total Dispense Button Count Meter (count after Dispense button is used)
-WRITE_TOTAL_RETRIES = 0x84 # Write Total Number of Retries
-READ_TOTAL_RETRIES = 0x85  # Read Total Number of Retries
-RESET_SETTINGS = 0x86      # Reset the card dispenser settings - no data or 1 byte unsigned CHAR
-WRITE_CARD_HOLD = 0x87     # Write “Hold the card in dispenser” set - 1 byte unsigned CHAR
-READ_CARD_HOLD = 0x88      # Read “Hold the card in dispenser” set  - no data
-WRITE_DELAY = 0x89         # Write “Delay Time” in secs             - 1 byte unsigned CHAR
-READ_DELAY = 0x8A          # Read “Delay Time” in secs              - no data
-RESET_EEPROM = 0x92        # Reset EEPROM values to default factory values - no data
+    DISPENSE = 0x80            # Dispense Card - no data
+    REQUEST_STATUS = 0x81      # Request Status - no data
+    READ_TOTAL_COUNT = 0x82    # Read Total Dispense Count Meter (count after API command is sent)
+    READ_TOTAL_BUTTON_COUNT = 0x83  # Read Total Dispense Button Count Meter (count after Dispense button is used)
+    WRITE_TOTAL_RETRIES = 0x84 # Write Total Number of Retries
+    READ_TOTAL_RETRIES = 0x85  # Read Total Number of Retries
+    RESET_SETTINGS = 0x86      # Reset the card dispenser settings - no data or 1 byte unsigned CHAR
+    WRITE_CARD_HOLD = 0x87     # Write “Hold the card in dispenser” set - 1 byte unsigned CHAR
+    READ_CARD_HOLD = 0x88      # Read “Hold the card in dispenser” set  - no data
+    WRITE_DELAY = 0x89         # Write “Delay Time” in secs             - 1 byte unsigned CHAR
+    READ_DELAY = 0x8A          # Read “Delay Time” in secs              - no data
+    RESET_EEPROM = 0x92        # Reset EEPROM values to default factory values - no data
 
-# eh?
-SET_MODE = 0x90      # Set Card Dispenser/Motor Control Mode - 1 byte unsigned CHAR
-MOTOR_ACTIVE = 0x91  # Motor Active Control                  - 2 bytes unsigned CHAR
-READ_OPERATION = 0x92 # Read Motor Operation Type: Turn Counter/Timer - 2 bytes unsigned CHAR
-WRITE_OPERATION = 0x93 # Write Motor Operation Type: Turn Counter/Timer - 2 bytes unsigned CHAR
-READ_MOTOR_TURNS = 0x94 # Read Motor Number of Turns Value   - 4 bytes unsigned CHAR
-WRITE_MOTOR_TURNS = 0x95 # Write Motor Number of Turns Value - 4 bytes unsigned CHAR
-READ_TIMER = 0x96    # Read Motor Timer value                - 4 bytes unsigned CHAR
-WRITE_TIMER = 0x97   # Write Motor Timer value               - 4 bytes unsigned CHAR
-MOTOR_STATUS = 0x98  # Motor Control Status                  - 2 bytes unsigned CHAR
-MOVE_TO_HOME = 0x99  # Move Motor Position to “Home” Position- 2 bytes unsigned CHAR
-READ_ADJUST_TIME = 0x9A # Read Home Position Trim Adjustment Time - 4 bytes unsigned CHAR
-WRITE_ADJUST_TIME = 0x9B # Write Home Position Trim Adjustment Time - 4 bytes unsigned CHAR
-MOTOR_CONTROL_STATUS = 0x9F # 
+    # eh?
+    SET_MODE = 0x90      # Set Card Dispenser/Motor Control Mode - 1 byte unsigned CHAR
+    MOTOR_ACTIVE = 0x91  # Motor Active Control                  - 2 bytes unsigned CHAR
+    READ_OPERATION = 0x92 # Read Motor Operation Type: Turn Counter/Timer - 2 bytes unsigned CHAR
+    WRITE_OPERATION = 0x93 # Write Motor Operation Type: Turn Counter/Timer - 2 bytes unsigned CHAR
+    READ_MOTOR_TURNS = 0x94 # Read Motor Number of Turns Value   - 4 bytes unsigned CHAR
+    WRITE_MOTOR_TURNS = 0x95 # Write Motor Number of Turns Value - 4 bytes unsigned CHAR
+    READ_TIMER = 0x96    # Read Motor Timer value                - 4 bytes unsigned CHAR
+    WRITE_TIMER = 0x97   # Write Motor Timer value               - 4 bytes unsigned CHAR
+    MOTOR_STATUS = 0x98  # Motor Control Status                  - 2 bytes unsigned CHAR
+    MOVE_TO_HOME = 0x99  # Move Motor Position to “Home” Position- 2 bytes unsigned CHAR
+    READ_ADJUST_TIME = 0x9A # Read Home Position Trim Adjustment Time - 4 bytes unsigned CHAR
+    WRITE_ADJUST_TIME = 0x9B # Write Home Position Trim Adjustment Time - 4 bytes unsigned CHAR
+    MOTOR_CONTROL_STATUS = 0x9F # 
 
-# Reserved Response Codes
-ACK = 0x06  # Accepted/Positive Status
-NAK = 0x15  # Rejected/Negative Status
-INC = 0xFD  # Incomplete Command Packet
-UNR = 0xFE  # Unrecognized Command Packet
-CER = 0xFF  # Data Packet Checksum Error
+    # Reserved Response Codes
+    ACK = 0x06  # Accepted/Positive Status
+    NAK = 0x15  # Rejected/Negative Status
+    INC = 0xFD  # Incomplete Command Packet
+    UNR = 0xFE  # Unrecognized Command Packet
+    CER = 0xFF  # Data Packet Checksum Error
 
-# status values
-READY = 0x30         # Ready to dispense the card
-BUSY  = 0x31         # The card dispenser is busy dispensing the card
-EMPTY = 0x32         # No cards inside the card dispenser stack
-STUCK = 0x33         # Card is jammed inside the card dispenser
-CARD_HOLD = 0x34     # Card is dispensed, but not yet removed
-DISABLED = 0x35
-CHECK_SENSORS = 0x36
-LOW_CARD_DETECTED = 0x37
+    # status values
+    READY = 0x30         # Ready to dispense the card
+    BUSY  = 0x31         # The card dispenser is busy dispensing the card
+    EMPTY = 0x32         # No cards inside the card dispenser stack
+    STUCK = 0x33         # Card is jammed inside the card dispenser
+    CARD_HOLD = 0x34     # Card is dispensed, but not yet removed
+    DISABLED = 0x35
+    CHECK_SENSORS = 0x36
+    LOW_CARD_DETECTED = 0x37
 
-ser = serial.Serial(
-	port='/dev/tty.usbserial-0000201A',
-	baudrate=19200,
-        bytesize=8,
-        parity='N',
-        stopbits=1,
-        xonxoff=0,
-        rtscts=0,
-        timeout=1
-)
+    def __init__(self, port='/dev/tty.usbserial-0000201A'):
+        self.serial = serial.Serial(port=port,
+                                    baudrate=19200,
+                                    bytesize=8,
+                                    parity='N',
+                                    stopbits=1,
+                                    xonxoff=0,
+                                    rtscts=0,
+                                    timeout=1)
 
-ser.open()
+    def open(self):
+        return self.serial.open()
 
-def _checksum(packet):
-    '''calculate the XOR checksum of a packet in string format'''
-    xorsum = 0
-    for s in packet:
-        xorsum ^= ord(s)
-    return xorsum
+    def close(self):
+        return self.serial.close()
 
-def _printpacket(packet):
-    packetprint = ''
-    for s in packet:
-        packetprint += hex(ord(s)) + ' '
-    print(packetprint)
+    def inWaiting(self):
+        return self.serial.inWaiting()
 
+    def _checksum(self, packet):
+        '''calculate the XOR checksum of a packet in string format'''
+        xorsum = 0
+        for s in packet:
+            xorsum ^= ord(s)
+        return xorsum
 
-def receivepacket():
-    bytes = []
-    endofpacket = False
-    while not endofpacket and ser.inWaiting():
-        byte = ser.read()
-        if byte != '':
-            bytes.append(byte)
-        if byte == ETX: endofpacket = True
-    # don't forget the checksum...
-    if ser.inWaiting():
-        bytes.append(ser.read())
-    return bytes
+    def _printpacket(self, packet):
+        packetprint = ''
+        for s in packet:
+            packetprint += hex(ord(s)) + ' '
+        print(packetprint)
 
 
-def _matchchecksum(packet):
-    receivedchecksum = ord(packet[-1])
-    print packet[0:-1]
-    calculatedchecksum  = _checksum(packet[0:-1])
-    # TODO perhaps this should throw an Exception when the checksums don't match?
-    if receivedchecksum == calculatedchecksum:
-        return True
-    else:
-        print 'CHECKSUM ERROR' + str(receivedchecksum) + ' != ' + str(calculatedchecksum)
-        return False
+    def receivepacket(self):
+        bytes = []
+        endofpacket = False
+        while not endofpacket and self.serial.inWaiting():
+            byte = self.serial.read()
+            if byte != '':
+                bytes.append(byte)
+            if byte == Vendapin.ETX: endofpacket = True
+        # don't forget the checksum...
+        if self.serial.inWaiting():
+            bytes.append(self.serial.read())
+        return bytes
 
 
-def parseresponsecode(packet):
-    '''parse the "command" byte from the response packet to get a "response code"'''
-    cmd = ord(packet[2])
-    if cmd == ACK: # Accepted/Positive Status
-        return True
-    elif cmd == NAK: # Rejected/Negative Status
-        print('NAK - Rejected/Negative Status')
-        return False
-    elif cmd == INC: # Incomplete Command Packet
-	raise Exception('INC - Incomplete Command Packet')
-    elif cmd == UNR: # Unrecognized Command Packet
-        raise Exception('UNR - Unrecognized Command Packet')
-    elif cmd == CER: # Data Packet Checksum Error
-        raise Exception('CER - Data Packet Checksum Error')
-    else:
-        raise Exception('Received bad CMD in response from card dispenser')
+    def _matchchecksum(self, packet):
+        receivedchecksum = ord(packet[-1])
+        print packet[0:-1]
+        calculatedchecksum  = self._checksum(packet[0:-1])
+        # TODO perhaps this should throw an Exception when the checksums don't match?
+        if receivedchecksum == calculatedchecksum:
+            return True
+        else:
+            print 'CHECKSUM ERROR' + str(receivedchecksum) + ' != ' + str(calculatedchecksum)
+            return False
 
 
-def parsedata(packet):
-    '''parse the data section of a packet, it can range from 0 to many bytes'''
-    data = []
-    datalength = ord(packet[3])
-    print 'datalength: ' + str(datalength)
-    position = 4
-    while position < datalength + 4:
-        data.append(packet[position])
-        position += 1
-    return data
+    def parseresponsecode(self, packet):
+        '''parse the "command" byte from the response packet to get a "response code"'''
+        cmd = ord(packet[2])
+        if cmd == Vendapin.ACK: # Accepted/Positive Status
+            return True
+        elif cmd == Vendapin.NAK: # Rejected/Negative Status
+            print('NAK - Rejected/Negative Status')
+            return False
+        elif cmd == Vendapin.INC: # Incomplete Command Packet
+            raise Exception('INC - Incomplete Command Packet')
+        elif cmd == Vendapin.UNR: # Unrecognized Command Packet
+            raise Exception('UNR - Unrecognized Command Packet')
+        elif cmd == Vendapin.CER: # Data Packet Checksum Error
+            raise Exception('CER - Data Packet Checksum Error')
+        else:
+            raise Exception('Received bad CMD in response from card dispenser')
 
 
-def parsestatus(data):
-    code = ord(data[0])
-    if code == READY:
-        print(' response: ready')
-    elif code == BUSY:
-        raise NakException('NAK response: busy', BUSY)
-    elif code == EMPTY:
-        raise NakException('NAK response: empty', EMPTY)
-    elif code == STUCK:
-        raise NakException('NAK response: stuck', BUSY)
-    elif code == CARD_HOLD:
-        raise NakException('NAK response: card hold', CARD_HOLD)
-    elif code == DISABLED:
-        raise NakException('NAK response: disabled', DISABLED)
-    elif code == CHECK_SENSORS:
-        raise NakException('NAK response: check sensors', CHECK_SENSORS)
-    elif code == LOW_CARD_DETECTED:
-        raise NakException('NAK response: low card detected', LOW_CARD_DETECTED)
-    else:
-        raise Exception('Bad response code: ' + str(code))
+    def parsedata(self, packet):
+        '''parse the data section of a packet, it can range from 0 to many bytes'''
+        data = []
+        datalength = ord(packet[3])
+        print 'datalength: ' + str(datalength)
+        position = 4
+        while position < datalength + 4:
+            data.append(packet[position])
+            position += 1
+        return data
 
 
-def parseresponse(packet):
-    if ord(packet[0]) != STX or ord(packet[-2]) != ETX:
-        print 'this is not a packet: ' + str(packet)
-        # this is not a packet, it could be the startup string, or a
-        # garbled package, or something else
-        return None
-    if not _matchchecksum(packet):
-        return None
-    if parseresponsecode(packet):
-        return True
-    else:
-        parsestatus(parsedata(packet))
+    def parsestatus(self, data):
+        code = ord(data[0])
+        if code == Vendapin.READY:
+            print(' response: ready')
+        elif code == Vendapin.BUSY:
+            raise NakException('NAK response: busy', Vendapin.BUSY)
+        elif code == Vendapin.EMPTY:
+            raise NakException('NAK response: empty', Vendapin.EMPTY)
+        elif code == Vendapin.STUCK:
+            raise NakException('NAK response: stuck', Vendapin.BUSY)
+        elif code == Vendapin.CARD_HOLD:
+            raise NakException('NAK response: card hold', Vendapin.CARD_HOLD)
+        elif code == Vendapin.DISABLED:
+            raise NakException('NAK response: disabled', Vendapin.DISABLED)
+        elif code == Vendapin.CHECK_SENSORS:
+            raise NakException('NAK response: check sensors', Vendapin.CHECK_SENSORS)
+        elif code == Vendapin.LOW_CARD_DETECTED:
+            raise NakException('NAK response: low card detected', Vendapin.LOW_CARD_DETECTED)
+        else:
+            raise Exception('Bad response code: ' + str(code))
 
 
-# <STX><ADD><CMD><LEN><DTA><ETX><CHK>
-def sendcommand(command, datalength=0, data=None):
-    '''send a packet in the vendapin format'''
-    packet = chr(STX) + chr(ADD) + chr(command) + chr(datalength)
-    if datalength > 0:
-        packet += chr(data)
-    packet += chr(ETX)
-    sendpacket = packet + chr(_checksum(packet))
-    _printpacket(sendpacket)
-    ser.write(sendpacket)
+    def parseresponse(self, packet):
+        if ord(packet[0]) != Vendapin.STX or ord(packet[-2]) != Vendapin.ETX:
+            print 'this is not a packet: ' + str(packet)
+            # this is not a packet, it could be the startup string, or a
+            # garbled package, or something else
+            return None
+        if not self._matchchecksum(packet):
+            return None
+        if self.parseresponsecode(packet):
+            return True
+        else:
+            self.parsestatus(self.parsedata(packet))
+
+
+    # <STX><ADD><CMD><LEN><DTA><ETX><CHK>
+    def sendcommand(self, command, datalength=0, data=None):
+        '''send a packet in the vendapin format'''
+        print chr(Vendapin.STX)
+        print chr(Vendapin.ADD)
+        packet = chr(Vendapin.STX) + chr(Vendapin.ADD) + chr(command) + chr(datalength)
+        if datalength > 0:
+            packet += chr(data)
+        packet += chr(Vendapin.ETX)
+        sendpacket = packet + chr(self._checksum(packet))
+        self._printpacket(sendpacket)
+        self.serial.write(sendpacket)
 
 
 #------------------------------------------------------------------------------#
 # for testing from the command line:
 def main(argv):
     # first clear out anything in the receive buffer
-    while ser.inWaiting():
-        print receivepacket()
-    sendcommand(REQUEST_STATUS)
+    v = Vendapin()
+    v.open()
+    while v.inWaiting():
+        print v.receivepacket()
+    v.sendcommand(Vendapin.REQUEST_STATUS)
     # wait for the reply
     waiting = True
     while waiting:
-        if ser.inWaiting() > 0:
+        if v.inWaiting() > 0:
             waiting = False
-    response = receivepacket()
-    if parseresponse(response):
-        sendcommand(DISPENSE)
+    response = v.receivepacket()
+    if v.parseresponse(response):
+        v.sendcommand(Vendapin.DISPENSE)
         # wait for the reply
         waiting = True
         while waiting:
-            if ser.inWaiting() > 0:
+            if v.inWaiting() > 0:
                 waiting = False
         # parse the reply
-        response = receivepacket()
-        parseresponse(response)
+        response = v.receivepacket()
+        v.parseresponse(response)
     else:
         print 'NOT READY'
-    print('inWaiting: ' + str(ser.inWaiting()))
-    ser.close()
+    print('inWaiting: ' + str(v.inWaiting()))
+    v.close()
     
 if __name__ == "__main__":
     main(sys.argv[1:])
